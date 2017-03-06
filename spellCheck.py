05c41804fa1d52b65f5e67ebfc4570f8c2845509
atchi102 <atchi102@mail.chapman.edu>
2017-03-05 22:44:04 -0800

    added feature to count how many errors were in each file

--- spellCheck.py
+++ spellCheck.py
@@ -1,11 +1,15 @@
+from __future__ import division
 from enchant.checker import SpellChecker
+
 import enchant
 import os
 
 
 
-def SpellCheck(contents, count, filename):
+def SpellCheck(contents, filename):
     print("FILE " + filename)
+
+    count = 0
 
     chkr = SpellChecker("en_US")
     chkr.set_text(contents)
@@ -23,27 +27,35 @@ def SpellCheck(contents, count, filename):
         contents = contents.replace(str(err.word),str(replaceWith))
         count+=1
 
-    return {'contents':contents, 'errors':count}
+    numWords = len(contents.split())
+    return {'contents':contents, 'errors':count, 'numWords':numWords}
 
 
 directory = "/Users/abigailatchison/Desktop/MLAT/RTFProcessing/EssaysTxt/test"
-numErrs = 0
+storePercent = open(os.path.join(directory + "/Output", "stats.txt"),'w')
 for filename in os.listdir(directory):
     if filename.endswith(".txt"):
         #print(os.path.join(directory, filename))
         log = open(os.path.join(directory, filename), 'r')
         fileContents = log.read()
 
-        results = SpellCheck(fileContents, numErrs, filename)
+        results = SpellCheck(fileContents, filename)
 
         newFile = open(os.path.join(directory + "/Output", filename),'w')
         newFile.truncate()
         newFile.write(results['contents'])
         newFile.close()
 
+        print("Total words: " + str(results['numWords']))
+        print("Total errors: " + str(results['errors']))
+        print("Percentage errors: " + str(results['errors']/results['numWords']))
+
+
+        storePercent.write(str(results['errors']/results['numWords'])+ "\n")
+
 
         continue
     else:
         continue
 
-print("NUM ERRORS: " + str(numErrs))
+storePercent.close()
